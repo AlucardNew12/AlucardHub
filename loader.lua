8095bc9d37637c09d00d5c47b8fef19c5544a694
AlucardNew12 <savinaxitada@gmail.com>
2025-01-05 22:55:14 -0300

    update loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+-- https://raw.githubusercontent.com/AlucardNew1/AlucardHub/refs/heads/main/loader.lua
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/AlucardNew12/AlucardHub/refs/heads/main/loader.lua",true))()
+if game.PlaceId == iddojogo then
+load
+end
