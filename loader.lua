17085f3aba62646af1bd3ed4336294ff5baea1bf
AlucardNew12 <savinaxitada@gmail.com>
2025-01-05 23:04:25 -0300

    update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,16 @@
 -- https://raw.githubusercontent.com/AlucardNew1/AlucardHub/refs/heads/main/loader.lua
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/AlucardNew12/AlucardHub/refs/heads/main/loader.lua",true))()
-if game.PlaceId == iddojogo then
-load
+local function LoadGame()
+  if game.PlaceId == iddojogo then
+    load
+  end
 end
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Hallow Hub | Evo Tycoon",
+   Icon = 17091459839,
+   LoadingTitle = "Hallow Hub",
+   LoadingSubtitle = "By Moligrafi",
+   Theme = "Amethyst"
+})
+
